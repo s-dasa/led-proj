d81680a0dc4f6b53a717127efb37b08c8dff65c8
Aniket Das <31552349+s-dasa@users.noreply.github.com>
2019-06-28 14:08:05 -0700

    friday leaving

--- led_database.py
+++ led_database.py
@@ -1,4 +1,11 @@
 pin = {"red": 27, "green": 22, "blue": 17, "white": 10}
 values = {"white":[255, 255, 255], "red": [255, 0, 0], "orange":[255, 127, 0], "yellow":[255, 255, 0], "green":[0, 255, 0], "blue":[0, 0, 255], "indigo":[39, 9, 5], "violet":[139, 0, 255]}
+rev_values = {"[255, 255, 255]":"white", "[255, 0, 0]":"red", "[255, 127, 0]":"orange", "[255, 255, 0]":"yellow", "[0, 255, 0]":"green", "[0, 0, 255]":"blue", "[39, 9, 5]":"indigo", "[139, 0, 255]":"violet"}
 def getValue(color):
     return values[color]
+def getValues:
+    return values
+def getPin(color):
+    return pin[color]
+def getColor(color)
+    return rev_values(color)
--- ledcontroller.py
+++ ledcontroller.py
@@ -1,17 +1,15 @@
 from gpiozero import *
 import RPi.GPIO as GPIO
 from time import sleep
-import led_database
+from led_database import *
 class controller:
+    
     color = input("Enter your desired color (from ROYGBIV and White) or its RGB value in the format: [R, G, B].")
-    led_database.database.retrievePin(color)
     if (color.isdigit()):
-        print("it's a number") 
+        print("it's a number")
+        color = led_database.getColor(color)
     elif (isinstance(color, str)):
-        if color in led_database.database.values.keys():
-                currentrgb = led_database.database.getValues(color)
-        else:
-                print("That's not one of the colors I know! Try again!") 
+        color=color.lower()
     else:
         print("That's not a valid input!")
         #continue
@@ -19,7 +17,7 @@ class controller:
     speed = int(input("And how fast do you want it to blink"))
 #color = input("Which color LED do you want to light? (Red/Green)")
     rate = 1/speed
-    led = LED(led_database.database.retrievePin(color))
+    led = LED(led_database.getPin(color))
     for x in range (times):
         sleep(rate)
         led.on()
