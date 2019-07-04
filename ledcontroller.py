465a547a4c167ee1f4c515931b1a4666c812f0d5
Aniket Das <31552349+s-dasa@users.noreply.github.com>
2019-07-04 13:01:44 -0700

    Add files via upload

--- ledcontroller.py
+++ ledcontroller.py
@@ -2,24 +2,55 @@ from gpiozero import *
 import RPi.GPIO as GPIO
 from time import sleep
 from led_database import *
-class controller:
-    
-    color = input("Enter your desired color (from ROYGBIV and White) or its RGB value in the format: [R, G, B].")
-    if (color.isdigit()):
-        print("it's a number")
-        color = led_database.getColor(color)
-    elif (isinstance(color, str)):
-        color=color.lower()
-    else:
-        print("That's not a valid input!")
-        #continue
-    times = int(input("How many times do you want it to blink?"))
-    speed = int(input("And how fast do you want it to blink"))
-#color = input("Which color LED do you want to light? (Red/Green)")
-    rate = 1/speed
-    led = LED(led_database.getPin(color))
-    for x in range (times):
-        sleep(rate)
-        led.on()
-        sleep(rate)
-        led.off()
+#from tester import *
+class controller():
+    def __init__(self):
+        self.led_obj = led_class()
+        #self.my_tester = testing()
+        self.color = ""
+        self.times = 0
+        self.rate = 1
+        self.led_obj.setUp()
+        self= self
+        self.speed = 0
+
+    def userInput(self):
+        self.color = input("Enter your desired color (from ROYGBIV and White) or its RGB value in the format: [R, G, B].")
+        self.times
+        int(input("How many times do you want it to blink?"))
+        self.speed = int(input("And how fast do you want it to blink"))
+        if (isinstance(self.color, list) and self.led_obj.checkList(self.color)):
+            #print("it's a list")
+            self.color = self.led_obj.getColor(self.color)
+            print(self.color)
+        elif (isinstance(self.color, str) and self.led_obj.checkList(self.color)):
+            self.color=self.color.lower()
+            #print("It's a string")
+        else:
+            print("That's not a valid input!")
+            userInput()
+    def lightUp(self):
+        self.led_obj.setUp()
+        self.led_obj.lightUp(self.color)
+        #self.my_tester.powerOn(self.color)
+    def execution(self):
+        rate = 1/self.speed
+        #print(self.led_obj.getPin(self.color))
+        led = LED(self.led_obj.getPin(self.color))
+        for x in range (self.times):
+            sleep(rate)
+            led.on()
+            sleep(rate)
+            led.off()
+        again = input("Again? Y or N")
+        if (again == "Y"):
+            userInput(self)
+        else: print("Thank you for using the LedController. Have a nice day")
+    """
+    def main(self):
+        userInput(self)
+        execution(self)
+    main(self)
+    """
+#ontrol = controller()
+c = controller()
